fb90cf25a99f9c5e76e43e2d2db3eec2788683be
sdt2022-code <sari.tarabay@mail.utoronto.com>
2024-08-18 15:21:59 -0400

    part revision testing script

--- Functions and Triggers/process_part_rev_audit_fucntion.sql
+++ Functions and Triggers/process_part_rev_audit_fucntion.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE FUNCTION process_part_rev_audit() RETURNS TRIGGER AS $part_rev
         -- making use of the special variable TG_OP to work out the operation.
         --
         IF (TG_OP = 'DELETE') THEN
-            INSERT INTO part_rev_audit SELECT 'D', OLD.part_num , OLD.part_revision_level, now(), OLD.change_desciption;
+            INSERT INTO part_rev_audit SELECT 'D', OLD.part_num , OLD.part_revision_level, now(), NULL;
 
         ELSIF (TG_OP = 'UPDATE') THEN
             INSERT INTO part_rev_audit SELECT 'U', OLD.part_num, OLD.part_revision_level, now(), NEW.change_desciption;
@@ -19,6 +19,6 @@ CREATE OR REPLACE FUNCTION process_part_rev_audit() RETURNS TRIGGER AS $part_rev
     END;
 $part_rev_audit$ LANGUAGE plpgsql;
 
-CREATE TRIGGER part_rev_audit
+CREATE OR REPLACE TRIGGER part_rev_audit
 AFTER INSERT OR UPDATE OR DELETE ON revision_db
     FOR EACH ROW EXECUTE FUNCTION process_part_rev_audit();
--- Testing Scripts/insert_into_parts_db.sql
+++ Testing Scripts/insert_into_parts_db.sql
@@ -9,24 +9,47 @@ INSERT INTO parts_db(
     unit_cost, 
     inventory_total_quantity, 
     inventory_tolerance, 
-    part_is_purchased, 
-    avg_time_to_complete, 
+    part_is_purchased,
+    avg_time_to_complete,
     unit_sales_price
     )
 
 VALUES (
-    '10-002', 
-	ARRAY['High-quality plastic', 'Heat-resistant'],
-	ARRAY['plastic', 'heat-resistant', 'industrial'],
-	'A',
-    'MFG-1002',  
-    123, 
-    INTERVAL '9 days',  
-    6.75,  
-    500,  
-    5,  
-    TRUE, 
-    INTERVAL '5 weeks',  
-    25.99
-
+    'A002', 
+	'Aluminum part for rice cooker cover',
+	ARRAY['Cover', 'Aluminum'],
+	'B',
+    NULL,  
+    NULL, 
+    NULL,  
+    10,  
+    4,  
+    0,  
+    FALSE,
+    INTERVAL '1 weeks',
+    45.99
 );
+
+
+
+INSERT INTO revision_db (
+    part_revision_level,
+    part_num,
+    rev_date_created,
+    revision_is_approved,
+    revision_approved_by,
+    revision_date_approval,
+    rev_docs,
+    change_description
+  )
+VALUES (
+    'B',
+    'A002',
+    NOW() - INTERVAL' 2 days',
+    TRUE,
+    'Manager',
+    NOW(),
+    'C:\Users\sari_\Desktop\AI Business\SaaS\Postgre Database Dev',
+    'Initial upload to system'
+  );
+ 
