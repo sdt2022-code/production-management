0a0a88b884727ed484d77f281021ff3cc286ff12
sdt2022-code <sari.tarabay@mail.utoronto.com>
2024-08-18 12:40:07 -0400

    fixed field consistency in function

--- Functions and Triggers/populate_part_manufacturers.sql
+++ Functions and Triggers/populate_part_manufacturers.sql
@@ -1,11 +1,22 @@
-CREATE FUNCTION populate_part_manufacturers()
+CREATE OR REPLACE FUNCTION populate_part_manufacturers()
 RETURNS TRIGGER AS $$
+
+DECLARE 
+
+manuf_name VARCHAR(30);
 
 BEGIN 
 
- IF (NEW.part_type = TRUE) THEN
+ IF (NEW.part_is_purchased = TRUE) THEN
+
+ SELECT m.manufacturer_name 
+ INTO manuf_name 
+ FROM manufacturers_db AS m 
+ WHERE m.manufacturer_id = NEW.manufacturer_id;
+
+
  INSERT INTO part_manufacturers(part_num , manuf_1, manuf_1_part_no)
- VALUES (NEW.part_num, NEW.manuf_name, NEW.manufacturer_part_no) ;
+ VALUES (NEW.part_num, manuf_name , NEW.manufacturer_part_no) ;
 
  END IF;
 
@@ -15,7 +26,7 @@ $$ LANGUAGE plpgsql;
 
 COMMENT ON FUNCTION populate_part_manufacturers() IS 'This function populates the part_num, manuf_1_name and manuf_1_part_no in part_manufacturers once a new part purchased part is created in parts_db and a manufacturer name and number is associated with the part.';
 
-CREATE TRIGGER populate_part_manufacturers_trigger
+CREATE OR REPLACE TRIGGER populate_part_manufacturers_trigger
 AFTER INSERT ON parts_db
 FOR EACH ROW 
 EXECUTE FUNCTION populate_part_manufacturers();
