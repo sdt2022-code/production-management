1cb4c99cca02a1cd1ff9ab2d0e069e7942d403e8
sdt2022-code <sari.tarabay@mail.utoronto.com>
2024-08-19 21:49:26 -0400

    Fixed bug in pulling part specs

--- Functions and Triggers/populate_part_desc_price_function.sql
+++ Functions and Triggers/populate_part_desc_price_function.sql
@@ -2,16 +2,16 @@ CREATE OR REPLACE FUNCTION populate_part_desc_price()
 RETURNS TRIGGER AS $$
 BEGIN
 	
-   IF EXISTS (SELECT 1 FROM parts_db WHERE part_num = NEW.part_num) THEN
-	SELECT part_description , part_unit_cost INTO NEW.part_description , 		NEW.unit_price
+   IF (NEW.part_num IS NOT NULL) THEN
+	SELECT part_description , unit_cost INTO NEW.part_description , NEW.unit_price
 	FROM parts_db 
 	WHERE part_num = NEW.part_num;
 
 
-   ELSIF EXISTS (SELECT 1 FROM assemblies WHERE assembly_num = NEW.part_num) THEN
-	SELECT assembly_description , assembly_sales_price INTO 		NEW.part_description, NEW.unit_price
+   ELSIF (NEW.assembly_num IS NOT NULL) THEN
+	SELECT assembly_description , assembly_sales_price INTO NEW.part_description, NEW.unit_price
 	FROM assemblies
-	WHERE assembly_num = NEW.part_num;
+	WHERE assembly_num = NEW.assembly_num;
 
    ELSE
 	RAISE EXCEPTION 'part_num % does not exist in parts or assemblies', NEW.part_num;
@@ -24,10 +24,10 @@ END;
 $$LANGUAGE plpgsql;
 
 
-COMMENT ON FUNCTION populate_part_description() IS 'This function pulls the unit_price and part_description from parts_description and populates the fields of unit_price and part_description in the sales order database.';
+COMMENT ON FUNCTION populate_part_desc_price() IS 'This function pulls the unit_price and part_description from parts_description and populates the fields of unit_price and part_description in the sales order database.';
 
 
-CREATE TRIGGER before_insert_or_update_sales
+CREATE OR REPLACE TRIGGER before_insert_or_update_sales
 BEFORE INSERT OR UPDATE ON sales_orders_db
 FOR EACH ROW
 EXECUTE FUNCTION populate_part_desc_price();
