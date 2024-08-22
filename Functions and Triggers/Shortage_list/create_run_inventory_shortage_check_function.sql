45d336cb222595bac2e48bda833db9a5dc889180
sdt2022-code <sari.tarabay@mail.utoronto.com>
2024-08-22 19:10:39 -0400

    fixed variables and function IF conditions

--- Functions and Triggers/Shortage_list/create_run_inventory_shortage_check_function.sql
+++ Functions and Triggers/Shortage_list/create_run_inventory_shortage_check_function.sql
@@ -1,3 +1,4 @@
+
 CREATE OR REPLACE FUNCTION run_inventory_shortage_check(specific_job_id INTEGER)
 RETURNS TABLE (job_id INTEGER ,part_num VARCHAR, part_description TEXT, qty_in_stock INTEGER, lead_time INTERVAL) 
 LANGUAGE plpgsql
@@ -7,17 +8,25 @@ AS $$
 DECLARE
 
  check_part_num VARCHAR(30);
+ is_confirmed BOOLEAN;
+ job_part_num VARCHAR(30);
+ job_assembly_num BIGINT;
 
 BEGIN
-   IF NEW.is_confirmed = TRUE THEN
 
-   	IF NEW.part_num IS NOT NULL THEN
+	SELECT j.job_is_confirmed, j.part_num, j.assembly_num INTO is_confirmed, job_part_num, job_assembly_num
+	FROM jobs_db AS j
+	WHERE j.job_num = specific_job_id;
+
+   IF is_confirmed = TRUE THEN
+
+   	IF job_part_num IS NOT NULL THEN
 
 		RETURN QUERY
 		SELECT * FROM get_single_part_shortage(specific_job_id);
 	
 
-	ELSEIF NEW.assembly_num IS NOT NULL THEN
+	ELSEIF job_assembly_num IS NOT NULL THEN
 	
 		RETURN QUERY 
 		SELECT * FROM get_shortage_parts(specific_job_id);
