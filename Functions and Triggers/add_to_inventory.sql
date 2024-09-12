0762531f13470ddf49be45ec57e4d5d62a2da453
sdt2022-code <sari.tarabay@mail.utoronto.com>
2024-09-11 22:21:19 -0400

    function captures average cost now

--- Functions and Triggers/add_to_inventory.sql
+++ Functions and Triggers/add_to_inventory.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE FUNCTION add_to_inventory(target_part_num VARCHAR , additional_quantity INTEGER)
+CREATE OR REPLACE FUNCTION add_to_inventory(target_part_num VARCHAR , additional_quantity INTEGER, new_unit_cost NUMERIC(10,3))
 RETURNS VOID AS $$
 
 BEGIN
     UPDATE inventory_parts_db
-    SET qty_in_stock = qty_in_stock + additional_quantity , last_updated = NOW()
+    SET qty_in_stock = qty_in_stock + additional_quantity , last_updated = NOW(), unit_cost = (unit_cost + new_unit_cost)/2
     WHERE part_num = target_part_num;
 
 END;
 $$ LANGUAGE plpgsql;
 
 COMMENT ON FUNCTION add_to_inventory(target_part_num VARCHAR, additional_quantity INTEGER) IS 'This function adds a specific
-quantity to a part in inventory.'
+quantity to a part in inventory and udptes the unit_cost of the part to be the average.'
