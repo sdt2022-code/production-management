deafd4b426d7a91f0c941540b64fcc6ca504e862
sdt2022-code <sari.tarabay@mail.utoronto.com>
2024-09-12 21:49:58 -0400

    add address information as seperate columns and not JSON

--- Tables/create_customer_db.sql
+++ Tables/create_customer_db.sql
@@ -5,5 +5,9 @@ CREATE  TABLE customer_db (
 	sales_contact_name   varchar(100),
 	sales_contact_email  varchar,
 	sales_contact_phone  integer,
-	customer_po_box      integer
+	address_street_1	TEXT,
+	address_street_2	TEXT,
+	address_city	TEXT,
+	address_state	TEXT,
+	address_zip	INTEGER
  );
